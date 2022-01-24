8844bc40888997c70fe9339eb800a4d0d391f9fa
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2022-01-24 14:06:08 -0500

    created a 8*8 board

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+const width = 8
+const candyColors = [
+  'blue', 
+  'green',
+  'orange',
+  'purple', 
+  'red', 
+  'yellow'
+]
 
 
+const App = () => {
 
-function App() {
+  const createBoard = () => {
+    const randomColorArrangement = []
+    for(let i=0; i < width * width; i++){
+      const randomNumberFrom0to5 = Math.floor(Math.random() * candyColors.length)
+      const randomColor = candyColors[randomNumberFrom0to5]
+      randomColorArrangement.push(randomColor)
+    }
+  }
+
+  createBoard()
+
   return (
     <div className="App">
     </div>
